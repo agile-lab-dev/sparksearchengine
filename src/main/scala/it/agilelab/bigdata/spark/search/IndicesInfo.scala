8e8bfda4f76b9bdd206d5c42fc1db4f5cf8183bd
Nicolò Bidotti <nicolo.bidotti@gmail.com>
2017-09-18 12:47:24 +0200

    Add pretty print methods

--- src/main/scala/it/agilelab/bigdata/spark/search/IndicesInfo.scala
+++ src/main/scala/it/agilelab/bigdata/spark/search/IndicesInfo.scala
@@ -3,8 +3,32 @@ package it.agilelab.bigdata.spark.search
 
 /** Global information about the indices.
 	*/
-case class IndicesInfo(numIndices: Int, numDocuments: Long, sizeBytes: Long, indicesInfo: List[IndexInfo])
+case class IndicesInfo(numIndices: Int, numDocuments: Long, sizeBytes: Long, indicesInfo: List[IndexInfo]) {
+	def prettyToString(): String = {
+		val localIndicesInfos = indicesInfo.zipWithIndex map {
+			case (indexInfo, indexNumber) =>
+				s"""|   index $indexNumber info:
+						|     num segments : ${indexInfo.numSegments}
+				    |     num documents: ${indexInfo.numDocuments}
+			      |     size bytes   : ${indexInfo.sizeBytes}""".stripMargin
+		}
+		val globalIndicesInfo =
+			s"""Indices info:
+					| num indices  : $numIndices
+					| num documents: $numDocuments
+		      | size bytes   : $sizeBytes""".stripMargin
+		
+		globalIndicesInfo + "\n" + localIndicesInfos.mkString("\n")
+	}
+}
 
 /** Information about an index.
 	*/
-case class IndexInfo(numDocuments: Int, sizeBytes: Long, numSegments: Int)
+case class IndexInfo(numDocuments: Int, sizeBytes: Long, numSegments: Int) {
+	def prettyToString(): String = {
+			s"""|Index info:
+			    |   num segments : $numSegments
+			    |   num documents: $numDocuments
+			    |   size bytes   : $sizeBytes""".stripMargin
+	}
+}
